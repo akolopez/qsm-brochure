cdb9973d9385952cf27660f33dc0f75e7f2db7ee
Miguel Elias Lee Lopez <qsm.miguel@gmail.com>
2017-01-22 12:46:42 -0800

    Remove carousel from about page.

--- src/about/about-page.js
+++ src/about/about-page.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 
 import ParagraphWithTitle from '../widgets/paragraph-with-title'
-import MyCarousel from '../widgets/my-carousel'
 
 class AboutPage extends Component {
   render() {
     return (
       <div>
-        <MyCarousel pictures={['sample', 'poultry3_dyphba', 'poultry_uc4qd3']}/>
         <ParagraphWithTitle title="Profile" body="
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rraesent eu mauris eu mauris.
 Morbi posuere sapien dui, at vestibulum leo ullamcorper in.
